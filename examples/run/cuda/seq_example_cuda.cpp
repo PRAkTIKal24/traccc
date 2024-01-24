6c547f1b4b3113dd74ad4c5e6ff8b91ae2fa2654
Pratik Jawahar <prjawaha@lxplus913.cern.ch>
2024-01-24 08:11:20 +0100

    adding instantiation for track finder function

--- examples/run/cuda/seq_example_cuda.cpp
+++ examples/run/cuda/seq_example_cuda.cpp
@@ -35,6 +35,22 @@
 #include "ActsExamples/GenericDetector/GenericDetectorElement.hpp"
 #include "ActsExamples/GenericDetector/ProtoLayerCreatorT.hpp"
 
+#include "Acts/Definitions/Direction.hpp"
+#include "Acts/EventData/MultiTrajectory.hpp"
+#include "Acts/EventData/TrackContainer.hpp"
+#include "Acts/EventData/TrackStatePropMask.hpp"
+#include "Acts/EventData/VectorMultiTrajectory.hpp"
+#include "Acts/EventData/VectorTrackContainer.hpp"
+#include "Acts/Propagator/EigenStepper.hpp"
+#include "Acts/Propagator/Navigator.hpp"
+#include "Acts/Propagator/Propagator.hpp"
+#include "Acts/TrackFinding/CombinatorialKalmanFilter.hpp"
+#include "Acts/TrackFitting/GainMatrixSmoother.hpp"
+#include "Acts/TrackFitting/GainMatrixUpdater.hpp"
+#include "Acts/Utilities/Logger.hpp"
+#include "ActsExamples/EventData/Track.hpp"
+#include "ActsExamples/TrackFinding/TrackFindingAlgorithm.hpp"
+
 // VecMem include(s).
 #include <vecmem/memory/cuda/device_memory_resource.hpp>
 #include <vecmem/memory/cuda/host_memory_resource.hpp>
@@ -47,7 +63,29 @@
 #include <iomanip>
 #include <iostream>
 
+#include <algorithm>
+#include <map>
+#include <memory>
+#include <utility>
+#include <vector>
+
+
 namespace po = boost::program_options;
+
+namespace {
+
+using Updater = Acts::GainMatrixUpdater;
+using Smoother = Acts::GainMatrixSmoother;
+
+using Stepper = Acts::EigenStepper<>;
+using Navigator = Acts::Navigator;
+using Propagator = Acts::Propagator<Stepper, Navigator>;
+using CKF =
+    Acts::CombinatorialKalmanFilter<Propagator, Acts::VectorMultiTrajectory>;
+
+using TrackContainer =
+    Acts::TrackContainer<Acts::VectorTrackContainer,
+                         Acts::VectorMultiTrajectory, std::shared_ptr>;
 
 int seq_run(const traccc::full_tracking_input_config& i_cfg,
             const traccc::common_options& common_opts,
@@ -129,6 +167,21 @@ int seq_run(const traccc::full_tracking_input_config& i_cfg,
     // return the pair of geometry and empty decorators
     auto GenericDetector =  std::make_pair<TrackingGeometryPtr, ContextDecorators>(
       std::move(gGeometry), std::move(gContextDecorators));
+
+    // Instantiate Track Finding function
+
+    Stepper stepper(std::move(magneticField));
+    Navigator::Config cfg{std::move(trackingGeometry)};
+    cfg.resolvePassive = false;
+    cfg.resolveMaterial = true;
+    cfg.resolveSensitive = true;
+    Navigator navigator(cfg, logger.cloneWithSuffix("Navigator"));
+    Propagator propagator(std::move(stepper), std::move(navigator),
+                          logger.cloneWithSuffix("Propagator"));
+    CKF trackFinder(std::move(propagator), logger.cloneWithSuffix("Finder"));
+
+    // build the track finder functions. owns the track finder object.
+    auto tf = std::make_shared<TrackFinderFunctionImpl>(std::move(trackFinder));
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
 
@@ -394,6 +447,9 @@ int seq_run(const traccc::full_tracking_input_config& i_cfg,
     std::cout << "==>Elapsed times...\n" << elapsedTimes << std::endl;
 
     return 0;
+
+} // namespace
+
 }
 
 // The main routine
